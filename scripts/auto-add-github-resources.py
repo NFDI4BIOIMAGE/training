18c87b1afb330ef9cb7bda5daa5c57604be25bd2
Robert Haase <haesleinhuepf@users.noreply.github.com>
2024-10-28 14:29:53 +0100

    Improve documentation / todos for maintainers

--- scripts/auto-add-github-resources.py
+++ scripts/auto-add-github-resources.py
@@ -102,7 +102,12 @@ def complete_github_data(github_repo_url):
     entry['publication_date'] = get_publication_date(repo)
 
     # Tags: always add the message "Dear users, please add tags."
-    entry['tags'] = "Dear users, please add tags."
+    entry['tags'] = """This is an automatically created pull-request. Before merging this, please make sure
+* [ ] has proper `tags` set.
+* [ ] has reasonable content `type` depending on the content of the github repository.
+
+ [Read more](https://nfdi4bioimage.github.io/training/contributing/format.html) 
+    """
 
     # Type: Always set this field to "GitHub Repository"
     entry['type'] = "GitHub Repository"
