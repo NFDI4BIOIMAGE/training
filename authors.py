733ee63bffc8d034e1f1395c57a37b2df4ba234f
Mara Lampert <mara.lampert@mailbox.tu-dresden.de>
2025-01-07 10:53:14 +0100

    create authors.py and import into download_statistics.ipynb #334

---
+++ authors.py
@@ -0,0 +1,11 @@
+our_authors = [
+  "Haase, Robert",
+  "Lampert, Mara", 
+  "Huang, Yixin", 
+  "Gihlein, Lea", 
+  "Massei, Riccardo", 
+  "Caporal, Clement", 
+  "Wetzker, Conni", 
+  "Zoccoler, Marcelo", 
+  "Schrader, Andrea"
+]
