d4c7a7d734f4c90c97fb8f9ab1b786676345c77f
Robert Haase <haesleinhuepf@users.noreply.github.com>
2025-01-17 10:44:29 +0100

    update our-authors list and reran notebook

---
+++ scripts/authors.py
@@ -0,0 +1,33 @@
+our_authors = [
+  "Haase, Robert",
+  "Lampert, Mara", 
+  "Huang, Yixin", 
+  "Gihlein, Lea", 
+  "Massei, Riccardo", 
+  "Caporal, Clement", 
+  "Wetzker, Conni", 
+  "Zoccoler, Marcelo", 
+  "Schrader, Andrea",
+  "Wendt, Jens",
+  "Zobel, Thomas",
+  "Wetzker, Cornelia",
+  "Fuchs, Vanessa",  
+  "Müller, Maximilian",
+  "Ahmadi, Mohsen",
+  "Schmidt, Christian", 
+  "Weidtkamp-Peters, Stefanie",
+  "Kunis, Susanne",
+  "Boissonnet, Tom",
+  "Ferrando-May, Elisa",
+  "Moore, Josh",
+]
+
+temp = []
+for author in our_authors:
+    temp.append(author)
+    if "," in author:
+        lastname = author.split(",")[0].strip()
+        firstname = author.split(",")[1].strip()
+        temp.append(firstname + " " + lastname)
+
+our_authors = temp
