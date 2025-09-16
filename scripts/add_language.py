d6a9fa54c63031298da42e26c112932a4f4123b8
Robert Haase <haesleinhuepf@users.noreply.github.com>
2025-09-16 17:58:30 +0200

    only send pr if something changes

--- scripts/add_language.py
+++ scripts/add_language.py
@@ -33,14 +33,13 @@ def add_language_to_resources_yml(repository, file_path="resources/nfdi4bioimage
         print("language is", lang)
         return lang 
 
-    # Create a new branch
-    branch_name = create_branch(repository, parent_branch=parent_branch)
 
     # Load YAML file content
     content_file = get_file_in_repository(repository, parent_branch, file_path)
     content = yaml.safe_load(content_file.decoded_content)
 
     # Add language to resources without one
+    modified = False
     for resource in content.get("resources", []):
         if "description" in resource and len(resource["description"]) > 200: # without detailed description, we can't determine the language
             if "language" not in resource or len(resource["language"]) == 0:
@@ -50,15 +49,23 @@ def add_language_to_resources_yml(repository, file_path="resources/nfdi4bioimage
                 if "description" in resource:
                     text += resource["description"]
                 resource["language"] = detect_language(text)
+                if len(resource["language"]) > 0:
+                    modified = True
 
     # Write updated YAML content back to the branch
-    updated_content = yaml.dump(content, sort_keys=False, allow_unicode=True)
-    write_file(repository, branch_name, file_path, updated_content, commit_message="Add missing language to resources")
+    if modified:
+        # Create a new branch
+        branch_name = create_branch(repository, parent_branch=parent_branch)
+        updated_content = yaml.dump(content, sort_keys=False, allow_unicode=True)
+        write_file(repository, branch_name, file_path, updated_content, commit_message="Add missing language to resources")
 
-    # Send a pull request
-    pr_title = "Add missing language to resources"
-    pr_description = f"Adds missing language to resources in the {file_path}. "
-    send_pull_request(repository, branch_name, pr_title, pr_description)
+        # Send a pull request
+        pr_title = "Add missing language to resources"
+        pr_description = f"Adds missing language to resources in the {file_path}. "
+        pr_url = send_pull_request(repository, branch_name, pr_title, pr_description)
+        print(pr_url)
+    else:
+        print("No modifications made to the resources file")
 
 if __name__ == "__main__":
     import sys
